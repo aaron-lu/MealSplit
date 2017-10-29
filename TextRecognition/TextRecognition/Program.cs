d09033c00ce09f9f66a0f3d111d7b7434f241089
kevinjyee <kevinyee@utexas.edu>
2017-10-29 09:45:57 -0500

    first commit

---
+++ TextRecognition/TextRecognition/Program.cs
@@ -0,0 +1,212 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace TextRecognition
+{
+    static class Program
+    {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Replace the subscriptionKey string value with your valid subscription key.
+        const string subscriptionKey = "99111d8302ee4f60a5d8b114abdf531f";
+
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace 
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Optical Character Recognition:");
+            Console.Write("Enter the path to an image with text you wish to read: ");
+            string imageFilePath = Console.ReadLine();
+
+            // Execute the REST API call.
+            MakeOCRRequest(imageFilePath);
+
+            Console.WriteLine("\nPlease wait a moment for the results to appear. Then, press Enter to exit...\n");
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Gets the text visible in the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file.</param>
+        static async void MakeOCRRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters.
+            string requestParameters = "language=unk&detectOrientation=true";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+
+                ReceiptInfo currentReceipt =  JsonConvert.DeserializeObject<ReceiptInfo>(contentString);
+
+                List<string> Items = new List<string>();
+                List<Double> Price = new List<Double>();
+
+                foreach (Region reg in currentReceipt.regions)
+                {
+                   foreach(Line l in reg.lines)
+                    {
+                        String item = "";
+                        foreach(Word w in l.words)
+                        {
+                            double cost;
+
+                            item += w.text;
+
+                            if(Double.TryParse(w.text, out cost))
+                            {
+                                Price.Add(cost);
+                            }                  
+                        }
+                        double c2;
+                        if (!Double.TryParse(item, out c2))
+                        {
+                            Items.Add(item);
+                        }
+                        
+                    }
+                }
+
+                Console.WriteLine("\nItems:\n");
+
+                foreach (string it in Items)
+                {
+                    Console.Out.WriteLine(it);
+                }
+
+                Console.WriteLine("\nCost:\n");
+
+                foreach (double it in Price)
+                {
+                    Console.Out.WriteLine(it);
+                }
+
+            }
+        }
+
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ TextRecognition/TextRecognition/ReceiptInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRecognition
+{
+    public class ReceiptInfo
+    {
+        public string language { get; set; }
+        public double textAngle { get; set; }
+        public string orientation { get; set; }
+        public List<Region> regions { get; set; }
+    }
+
+    public class Word
+    {
+        public string boundingBox { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Line
+    {
+        public string boundingBox { get; set; }
+        public List<Word> words { get; set; }
+    }
+
+    public class Region
+    {
+        public string boundingBox { get; set; }
+        public List<Line> lines { get; set; }
+    }
+}
+
+
